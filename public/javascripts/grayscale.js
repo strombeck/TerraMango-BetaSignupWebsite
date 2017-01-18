3b9e3c36e3c632feb64119fd7b5d73ed91e18470
DaveMatthews89 <dj@strombeck.com>
2017-01-18 10:09:59 -0500

    Fixed Validation Issues
    When submitting the form previously, if the user entered invalid
    information, hit the submit button, and then corercted the information,
    the form wouldn't submit, the server was hung. I've re-arranged the
    validation structure and it is working properly now.

--- public/javascripts/grayscale.js
+++ public/javascripts/grayscale.js
@@ -13,28 +13,38 @@ $(document).ready(function(){
         signupForm.addClass('hidden');
     }
     signupForm.submit(function(e){
-        var data = signupForm.serialize()
-        $.post(this.action, data, function(data){
-           signupForm.addClass('hidden');
-           itFailed.addClass('hidden');
-           introText.addClass('hidden');
-           msgSubmit.removeClass('hidden');
-           console.log(data);
-           console.log("Successful - posted to the database");
-           console.log(url);
-           if (url == "/?beta-signup=1"){
-            console.log("found beta signup in the success area = 1");
-            }
-        })
-        .fail(function(response) {
-            console.log('We are failures!!!');
-            itFailed.removeClass('hidden');
-            console.log(response);
-            console.log(url);
-            if (url == "/?beta-signup=400"){
-            console.log("found beta signup 400 in the failure area");
-            }
-            
+
+        var data = signupForm.serialize();
+        $.post(
+           this.action, 
+           data, 
+           function(data){//success
+               console.log(data);
+                if(data == "Success"){
+                    signupForm.addClass('hidden');
+                    itFailed.addClass('hidden');
+                    introText.addClass('hidden');
+                    msgSubmit.removeClass('hidden');
+                    console.log(data);
+                    console.log("Successful - posted to the database");
+                    console.log(url);
+                    if (url == "/?beta-signup=1"){
+                        console.log("found beta signup in the success area = 1");
+                    }
+                }
+                else{
+                    itFailed.text(data);
+                    itFailed.removeClass('hidden');
+                }
+           }
+        ).fail(
+            function(data) {
+                itFailed.text(data);
+                itFailed.removeClass('hidden');
+                
+                if (url == "/?beta-signup=400"){
+                    console.log("found beta signup 400 in the failure area");
+                }
         });
         e.preventDefault();
     });
--- routes/index.js
+++ routes/index.js
@@ -8,67 +8,42 @@ router.post('/%F0%9F%91%BD',function(req,res){
   var state=req.body.state;
   var phone=req.body.phone;
 
-  // if (!(validator.isEmail(email))) {
-  //   console.error("E-mail field didn't pass validation: " + email);
-  //   res.redirect("/?beta-signup=400")
-  //   // res.status(400).send('Bad email');
-  //   // response.send(String(444));
-  //   // res.status("test")
-  //   // res.writeHead(400, 'Please enter a valid e-mail address.')
-  //   // res.write("444");
-  //   // res.end(err);
-  //   // res.status(406).send('E-mail Not Acceptable')
-  //   // console.log(res.headersSent);
-  // }
 
-  // if (state.length > 2){
-  //   console.error("State field didn't pass validation: " + state);
-  //   res.redirect("/?beta-signup=401")
-  //   // res.end(err);
-  // }
-
-  //   if (phone != 0 && phone != 1){
-  //   console.error("Phone field didn't pass validation: " + phone);
-  //   res.redirect("/?beta-signup=402")
-  //   // res.end(err);
-  // }
-
-  getConnection((err, pg) => {
-    var headers = req.headers;
-    if (err) {
-      console.error(err);
-      res.end(err);
-    } else if (!(validator.isEmail(email))) {
-      res.sendStatus(998, "Bad E-mail");
-        // res.status('488');
-        // res.send('poooop');
-        // res.end('test');
+if (!(validator.isEmail(email))) {
+        res.send("Bad email");
         console.error("E-mail field didn't pass validation: " + email);
-        // res.redirect("/?beta-signup=400")
-    } else if (state.length > 2 || state.length == 0 ){
-        res.sendStatus(997, "State is invalid");
+    } 
+    else if (state.length > 2 || state.length == 0 ){
+        res.send("State is invalid");
         console.error("State field didn't pass validation: " + state);
-        // res.redirect("/?beta-signup=401")
-    } else if (phone != '0' && phone != '1'){
-        res.sendStatus(996, "You have a windows phone, don't you?");
+    } 
+    else if (phone != '0' && phone != '1'){
+        res.send("You have a windows phone, don't you?");
         console.error("Phone field didn't pass validation: " + phone);
-        // res.redirect("/?beta-signup=402")
-    }
+    } 
     else {
-      pg.client.query("INSERT into signup (email, state, phone_type) VALUES ($1, $2, $3 )", [email, state, phone], (err, result) => {
-        pg.done();
-        if (err) {
-          console.error(err);
-          res.redirect("/?beta-signup=0")
-        } else {
-          res.redirect("/?beta-signup=1") 
-          console.log(JSON.stringify(headers));
-          console.log("Made it to the database")
-          // headers["x-requested-with"] === "XMLHttpRequest"
-        }
-      });
+        getConnection((err, pg) => {
+          if (err) {
+            console.error(err);
+            res.end(err);
+          } 
+          var headers = req.headers;
+          console.log('made it to server')
+            pg.client.query("INSERT into signup (email, state, phone_type) VALUES ($1, $2, $3 )", [email, state, phone], (err, result) => {
+              pg.done();
+              if (err) {
+                console.error(err);
+                res.send("There was a problem saving your email to the server. Please try again later.");
+              } 
+              else {
+                console.log(JSON.stringify(headers));
+                console.log("Made it to the database");
+                res.send("Success"); 
+                // headers["x-requested-with"] === "XMLHttpRequest"
+              }
+            });
+        });
     }
-  });
 });
 
 module.exports = router;
